fc040feb85a846aff681e0df1380267575c6fe79
oww10 <dntjr2209@gmail.com>
2025-09-02 17:59:12 +0900

    lv2 기능구현입니다.
    <aside>
    🧑🏻‍💻 `UIStackView` 을 사용해서 4개의 버튼을 모아 가로 스택뷰 생성. 왼쪽과 같이 구성해보세요.
    
    - **`UIButton` 속성**
        - `font = .boldSystemFont(ofSize: 30)`
        - `backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)`
        - `height = 80`
        - `width = 80`
        - `layer.cornerRadius = 40`
    - **`horizontalStackView` 속성**
        - `axis = .horizontal`
        - `backgroundColor = .black`
        - `spacing = 10`
        - `distribution = .fillEqually`
    - **`horizontalStackView AutoLayout`**
        - height = 80
    </aside>

--- Delegate/SceneDelegate.swift
+++ Delegate/SceneDelegate.swift
@@ -9,7 +9,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = lv1ViewController()
+        window.rootViewController = lv2ViewController()
         
         self.window = window
         window.makeKeyAndVisible()
