4d3486e71ee724068c6b397cf50e1ecb19c4a5b4
oww10 <dntjr2209@gmail.com>
2025-09-03 17:48:27 +0900

    create Calculator File
    lv이 아닌 Calculator파일을 만들어 정리

---
+++ CalculatorApp/Calculator/Calculator.swift
@@ -0,0 +1,137 @@
+import UIKit
+import SnapKit
+
+class Calculator: UIViewController, makeLineButtons{
+    let resultText: UILabel = {
+        var text = UILabel()
+
+        text.text = "0"
+        text.textColor = .white
+        text.backgroundColor = .black
+        text.font = .boldSystemFont(ofSize: 60)
+        text.textAlignment = .right
+
+
+        return text
+    }()
+
+    func makeHorizontalStackView(_ views: [UIView]) -> UIStackView{
+        let stackView = UIStackView()
+
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .black
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+
+
+        for view in views {
+            stackView.addArrangedSubview(view)
+        }
+
+        return stackView
+    }
+
+    let VerticalStackView: UIStackView = {
+        let stackView = UIStackView()
+
+        stackView.axis = .vertical
+        stackView.backgroundColor = .black
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+
+        return stackView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+
+    @objc func buttonTapped(_ sender: UIButton){
+        
+        func calculate(expression: String) -> Int? {
+                let expression = NSExpression(format: expression)
+            if let result = expression.expressionValue(with: nil, context: nil) as? Int {
+                return result
+            } else {
+                return nil
+            }
+        }
+        
+        // setTitle을 가져오기
+        guard let buttonNum: String = sender.titleLabel?.text else{
+            return
+        }
+        let defaltTextNum = resultText.text ?? "0"
+        
+        if buttonNum == "AC"{
+            print(defaltTextNum)
+            resultText.text = "0"
+        } else {
+            if defaltTextNum == "0"{
+                resultText.text = buttonNum
+            } else {
+                resultText.text = defaltTextNum + buttonNum
+            }
+        }
+        if buttonNum == "="{
+            let result = calculate(expression: defaltTextNum)
+            resultText.text = result?.description
+        }
+    }
+
+
+
+    private func configureUI(){
+        view.backgroundColor = .black
+        view.addSubview(resultText)
+
+        let firstButtonsLine = makeButtonLine(buttonTitle: ["7","8","9","+"]
+                                              ,target: self, action: #selector(buttonTapped))
+        let secondButtonsLine = makeButtonLine(buttonTitle: ["4","5","6","-"]
+                                               ,target: self, action: #selector(buttonTapped))
+        let thirdButtonsLine = makeButtonLine(buttonTitle: ["1","2","3","*"]
+                                              ,target: self, action: #selector(buttonTapped))
+        let fourthButtonsLine = makeButtonLine(buttonTitle: ["AC","0","=","/"]
+                                               ,target: self, action: #selector(buttonTapped))
+
+        let firstVerticalView = makeHorizontalStackView(firstButtonsLine)
+        let secondVerticalView = makeHorizontalStackView(secondButtonsLine)
+        let thirdVerticalView = makeHorizontalStackView(thirdButtonsLine)
+        let fourthVerticalView = makeHorizontalStackView(fourthButtonsLine)
+
+
+        [firstVerticalView,secondVerticalView,thirdVerticalView,fourthVerticalView].forEach{
+            view.addSubview($0)
+        }
+
+        view.addSubview(VerticalStackView)
+        [firstVerticalView,secondVerticalView,thirdVerticalView,fourthVerticalView].forEach{
+            VerticalStackView.addArrangedSubview($0)
+        }
+
+
+        resultText.snp.makeConstraints{
+            make in
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-30)
+            make.top.equalToSuperview().offset(200)
+            make.height.equalTo(100)
+        }
+
+
+
+        VerticalStackView.snp.makeConstraints{
+            make in
+            make.width.equalTo(350)
+            make.top.equalTo(resultText.snp.bottom).offset(60)
+            make.centerX.equalToSuperview()
+        }
+
+    }
+
+}
+#Preview{
+    Calculator()
+}
--- CalculatorApp/Delegate/SceneDelegate.swift
+++ CalculatorApp/Delegate/SceneDelegate.swift
@@ -9,7 +9,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = lv7ViewController()
+        window.rootViewController = Calculator()
         
         self.window = window
         window.makeKeyAndVisible()
